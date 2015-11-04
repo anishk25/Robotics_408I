e4ab2ef1502636fbca3bc432daae732daa43d5e2
anishk25 <prgrmer25@gmail.com>
2015-11-04 14:30:38 -0500

    added dilation

--- imagProc/color_blob_detection.py
+++ imagProc/color_blob_detection.py
@@ -3,6 +3,26 @@
 
 import numpy as np
 import cv2
+import math
+
+COLOR_MIN = np.array([6,50,50]);
+COLOR_MAX = np.array([14,255,255]);
+
+MIN_CONTOUR_AREA = 1500
+
+CV_CAP_PROP_FRAME_WIDTH = 3
+CV_CAP_PROP_FRAME_HEIGHT = 4 
+
+FRAME_WIDTH = 640
+FRAME_HEIGHT = 480
+
+
+
+CAMERA_ANGLE_OF_VIEW_HORIZ = 53.5
+RADIANS_PER_PIX_X = math.radians(CAMERA_ANGLE_OF_VIEW_HORIZ)/FRAME_WIDTH
+DEGRESS_PER_PIX_X = CAMERA_ANGLE_OF_VIEW_HORIZ/FRAME_WIDTH
+CENTER_OF_FRAME = (FRAME_WIDTH/2,FRAME_HEIGHT/2)
+FONT = cv2.FONT_HERSHEY_SIMPLEX
 
 
 def getHSVChannel(img,index):
@@ -35,14 +55,18 @@ def getCircleFromContour(contour):
 	center,radius = cv2.minEnclosingCircle(approxCurve)
 	return center,radius
 
-def drawCentersOfContours(contours,frame):
+def drawAngleOfContours(contours,frame):
 	for contour in contours:
-		center,radius = getCircleFromContour(contour)
-		r_int = int(radius)
+		#center,radius = getCircleFromContour(contour)
+		moment = cv2.moments(contour)
+		center = (moment['m10']/moment['m00'],moment['m01']/moment['m00'])
+		dist_x = center[0]-CENTER_OF_FRAME[0]
+		angle = dist_x * DEGRESS_PER_PIX_X
 		center_in = (int(center[0]),int(center[1]))
-		cv2.circle(frame,center_in,2,(0,255,0),3)
+		cv2.putText(frame,str(angle),center_in,FONT, 1,(0,255,0),2)
+		#r_int = int(radius)
+		cv2.circle(frame,center_in,2,(255,0,0),3)
 
-	
 
 def convertRGBColorToHSV(colorScalar):
 	rgb_img = np.zeros((1,1,3),np.uint8)
@@ -52,22 +76,6 @@ def convertRGBColorToHSV(colorScalar):
 	return hsv_img[0,0]
 
 
-CV_CAP_PROP_FRAME_WIDTH = 3
-CV_CAP_PROP_FRAME_HEIGHT = 4 
-
-FRAME_WIDTH = 640
-FRAME_HEIGHT = 480
-
-# color order is B,G,R !!!
-#COLOR_MIN = convertRGBColorToHSV(np.array([5,50,50]))
-#COLOR_MAX = convertRGBColorToHSV(np.array([15,255,255]))
-
-COLOR_MIN = np.array([6,50,50]);
-COLOR_MAX = np.array([14,255,255]);
-
-
-MIN_CONTOUR_AREA = 1500
-
 
 videoCap = cv2.VideoCapture(0)
 videoCap.set(CV_CAP_PROP_FRAME_WIDTH,640)
@@ -76,6 +84,9 @@ videoCap.set(CV_CAP_PROP_FRAME_HEIGHT,480)
 if(videoCap.isOpened() != True):
 	videoCap.open()
 
+
+#kernel = np.array([[0,1,0],[1,1,1],[0,1,0]],np.uint8);
+kernel = np.ones((6,6),np.uint8)
 
 while(True):
 	ret,frame = videoCap.read()
@@ -88,17 +99,16 @@ while(True):
 
 	hThreshImg = cv2.inRange(blurredHImg,4,7)
 	sThreshImg = cv2.inRange(blurredSImg,155,255)
-
 	combImg = cv2.bitwise_and(hThreshImg,sThreshImg)
+	erodedDilImg = cv2.morphologyEx(combImg, cv2.MORPH_OPEN, kernel)
+	corners = cv2.cornerHarris(erodedDilImg,2,3,0.04);
 
-	#cannyImg = cv2.Canny(combImg,50,70)
-
-	contours, hierarchy = cv2.findContours(combImg,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	contours, hierarchy = cv2.findContours(erodedDilImg,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 	filterCont = filterContours(contours,MIN_CONTOUR_AREA);
-	drawCentersOfContours(filterCont,frame)
-	
-	#cv2.imshow('h_thresh',hThreshImg)
-	#cv2.imshow('s_thresh',sThreshImg)
+
+	#cv2.drawContours(frame,filterCont,-1,(0,255,0),2)
+	drawAngleOfContours(filterCont,frame)
+
 	cv2.imshow('frame',frame)
 	if(cv2.waitKey(1) & 0xFF == ord('q')):
 		break
